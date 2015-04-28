8779d9af3bc3d90fc169cf73793da91a7106cf0c
acburgos <alberto.casb@gmail.com>
2015-04-28 11:05:32 +0200

    1.0

--- recov3.go
+++ recov3.go
@@ -1,10 +1,6 @@
 package main
 
-/*
-POR HACER:
 
--Mejorar estilo (SetStyle es una puta mierda con el de go)
-*/
 import (
 	"flag"
 	"fmt"
@@ -16,12 +12,12 @@ import (
 
 var (
 	p = flag.String("p", "ninonino", "p:riemann,mandelbrot,saxpy,primes")
-	numPruebas int
-	tiempos []float64
+	numTest int
+	times []float64
 )
 
 func start() {
-	f, _ := ioutil.ReadFile("/home/acastano/GoLand/scripts/resultados.txt")
+	f, _ := ioutil.ReadFile("file to read")
 	f1 := strings.Split(string(f), "\n")
 	fmt.Println(f1)
 	f2 := make([]float64, len(f1)-1)
@@ -30,10 +26,10 @@ func start() {
 		f2[i], _ = strconv.ParseFloat(f1[i], 64)
 	}
 	fmt.Println("f2: ", f2, len(f2))
-	for i := 0; i < numPruebas; i++ {
-		tiempos[i] = tiempoMedio(f2[1*i : 1*i+1])
+	for i := 0; i < numTest; i++ {
+		times[i] = optime(f2[1*i : 1*i+1])
 	}
-	fmt.Println("tiempos", tiempos, "tiempos[0:4]", tiempos[0:4], "tiempos[4:8]", tiempos[4:8])
+	fmt.Println("times", times, "times[0:4]", times[0:4], "times[4:8]", times[4:8])
 }
 
 func plot() *gnuplot.Plotter {
@@ -49,7 +45,7 @@ func plot() *gnuplot.Plotter {
 	return p
 }
 
-func tiempoMedio(lista []float64) float64 {
+func optime(lista []float64) float64 {
 	var res float64
 	for i := 0; i < len(lista); i++ {
 		res1, _ := lista[i], 64
@@ -60,17 +56,17 @@ func tiempoMedio(lista []float64) float64 {
 }
 
 func main() {
-	numPruebas = 8
+	numTest = 8
 	flag.Parse()
-	tiempos = make([]float64, numPruebas)
+	times = make([]float64, numTest)
 
 	switch *p {
 	case "peter":
 		start()
 		p1 := plot()
 		p1.SetStyle("lines")
-		p1.PlotNd("yeahbitch", []float64{1, 2, 3, 4, 5, 6, 7}, []float64{8, 9, 15, 20, 50})
-		p1.PlotNd("yeahbitch", []float64{7, 6, 5, 4, 3, 2, 1}, []float64{50, 20, 20, 15, 8})
+		p1.PlotNd("testing", []float64{1, 2, 3, 4, 5, 6, 7}, []float64{8, 9, 15, 20, 50})
+		p1.PlotNd("testiung", []float64{7, 6, 5, 4, 3, 2, 1}, []float64{50, 20, 20, 15, 8})
 		p1.SetXLabel("operations")
 		p1.SetYLabel("time")
 		p1.SetZLabel("pet")
@@ -83,8 +79,8 @@ func main() {
 		start()
 		p1 := plot()
 		p1.SetStyle("lines")
-		p1.PlotXY([]float64{1e8, 1e9, 2e9, 5e9, 1e10}, tiempos[0:4], "Secuencial")
-		p1.PlotXY([]float64{1e8, 1e9, 2e9, 5e9, 1e10}, tiempos[4:8], "Paralelo")
+		p1.PlotXY([]float64{1e8, 1e9, 2e9, 5e9, 1e10}, times[0:4], "Sec")
+		p1.PlotXY([]float64{1e8, 1e9, 2e9, 5e9, 1e10}, times[4:8], "Parallel")
 		p1.SetXLabel("operations")
 		p1.SetYLabel("time")
 		p1.CheckedCmd("set terminal pdf")
@@ -97,8 +93,8 @@ func main() {
 		start()
 		p1 := plot()
 		p1.SetStyle("boxes")
-		p1.PlotXY([]float64{1, 4, 8, 12, 20}, tiempos[0:2], "Secuencial")
-		p1.PlotXY([]float64{0.25, 1, 2, 3, 5}, tiempos[3:5], "Paralelo")
+		p1.PlotXY([]float64{1, 4, 8, 12, 20}, times[0:2], "Sec")
+		p1.PlotXY([]float64{0.25, 1, 2, 3, 5}, times[3:5], "Parallel")
 		p1.SetXLabel("operations")
 		p1.SetYLabel("time")
 		p1.CheckedCmd("set terminal pdf")
@@ -111,8 +107,8 @@ func main() {
 		start()
 		p1 := plot()
 		p1.SetStyle("boxes")
-		p1.PlotXY([]float64{1, 4, 8, 12, 20}, tiempos[0:2], "Secuencial")
-		p1.PlotXY([]float64{0.25, 1, 2, 3, 5}, tiempos[3:5], "Paralelo")
+		p1.PlotXY([]float64{1, 4, 8, 12, 20}, times[0:2], "Sec")
+		p1.PlotXY([]float64{0.25, 1, 2, 3, 5}, times[3:5], "Parallel")
 		p1.SetXLabel("operations")
 		p1.SetYLabel("time")
 		p1.CheckedCmd("set terminal pdf")
@@ -125,8 +121,8 @@ func main() {
 		start()
 		p1 := plot()
 		p1.SetStyle("boxes")
-		p1.PlotXY([]float64{1, 4, 8, 12, 20}, tiempos[0:2], "Secuencial")
-		p1.PlotXY([]float64{0.25, 1, 2, 3, 5}, tiempos[3:5], "Paralelo")
+		p1.PlotXY([]float64{1, 4, 8, 12, 20}, times[0:2], "Sec")
+		p1.PlotXY([]float64{0.25, 1, 2, 3, 5}, times[3:5], "Parallel")
 		p1.SetXLabel("operations")
 		p1.SetYLabel("time")
 		p1.CheckedCmd("set terminal pdf")
@@ -136,6 +132,6 @@ func main() {
 		p1.CheckedCmd("q")
 
 	default:
-		fmt.Println("Fuck You!")
+		fmt.Println("<3")
 	}
 }
